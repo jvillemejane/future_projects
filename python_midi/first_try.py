9228a3b0896d045eb5a079b2ff1204282c5b815e
Julien Villemejane <julien.villemejane@institutoptique.fr>
2026-09-13 15:40:24 +0200

    Update MIDI python with Mido on Launchpad Pro

--- python_midi/first_try.py
+++ python_midi/first_try.py
@@ -1,15 +1,52 @@
 # MIDO package - First test
 
 import mido
+from mido import Message
+import time
+
+def all_black(midi_out):
+    for k in range(128):
+        msg = mido.Message('note_off', note=k, velocity=0, channel=0)
+        midi_out.send(msg)
 
 # Find MIDI devices (USB)
-print(mido.get_input_names())
-print(mido.get_output_names())
+print('MIDI Inputs')
+inputs = mido.get_input_names()
+for k, midi_in in enumerate(inputs):
+    print(f'({k+1}) {midi_in}')
+input_select = inputs[int(input('Select Midi input')) - 1]
 
-# Sending note
-out = mido.open_output('Launchpad Pro MIDI Out')
+print('MIDI Outputs')
+outputs = mido.get_output_names()
+for k, midi_out in enumerate(outputs):
+    print(f'({k+1}) {midi_out}')
+output_select = outputs[int(input('Select Midi output')) - 1]
 
-msg = mido.Message('note_on', note=60, velocity=127, channel=0)
-out.send(msg)
-# Cut the LED of Launchpad Pro
-out.send(mido.Message('note_off', note=60, velocity=0, channel=0))
+
+midi_out = mido.open_output(output_select)
+midi_in = mido.open_input(input_select)
+
+
+if input_select is not None and output_select is not None :
+    print('Test MIDI')
+        
+    all_black(midi_out)
+        
+    # Sending note
+    for k in range(128):
+        msg = mido.Message('note_on', note=k, velocity=k, channel=0)
+        midi_out.send(msg)
+        time.sleep(0.01)
+        
+## LISTENING MIDI IN
+with midi_in as port:
+    print(f"Écoute de : {midi_in}")
+
+    for msg in port:
+        print(msg)
+        
+        if msg.type == "control_change":
+            if msg.control == 98:  # User keys
+                break
+
+all_black(midi_out)
